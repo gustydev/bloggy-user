8ea74ccb17f7ebf761d7b326ac406ec6f47e51b3
gusty <gustavodiorlando@gmail.com>
2024-08-28 14:26:53 -0300

    Separate effects for fetching posts and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,12 @@
-import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [comments, setComments] = useState([])
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    let ignore = false;
-
-    async function fetchPosts() {
-      try {
-        const response = await fetch('https://cors-anywhere.herokuapp.com/https://bloggy.adaptable.app/api/v1/posts');
-        const posts = await response.json();
-  
-        if (!response.ok) {
-          throw new Error("Error fetching posts. Status: ", response.status)
-        }
-
-        setPosts(posts);
-      } catch (error) {
-        setError(error)
-        console.error(error)
-      }
-    }
-
-    async function fetchComments() {
-      try {
-        const response = await fetch('https://cors-anywhere.herokuapp.com/https://bloggy.adaptable.app/api/v1/comments?limit=3&sort=desc');
-        const comments = await response.json();
-  
-        if (!response.ok) {
-          throw new Error("Error fetching comments. Status: ", response.status)
-        }
-
-        setComments(comments);
-      } catch (error) {
-        setError(error)
-        console.error(error)
-      }
-    }
-
-    async function fetchData() {
-      await Promise.all([fetchPosts(), fetchComments()]);
-      setLoading(false)
-    }
-    
-    if (!ignore) {
-      fetchData();
-    }
-
-    return () => {
-      ignore = true;
-    }
-  }, [])
-
-  console.log(posts, comments)
-
   return (
     <>
       <div className="topBar">
         <h1>Bloggy</h1>
       </div>
-      {loading ? <p>Loading...</p> : error ? <p>A network error has occured</p> : <Outlet context={[posts, comments]}></Outlet>}
+      <Outlet></Outlet>
     </>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,95 @@
-import { useOutletContext } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [posts, comments] = useOutletContext();
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([])
+    const [error, setError] = useState(null)
+    const [loadingPosts, setLoadingPosts] = useState(true)
+    const [loadingComments, setLoadingComments] = useState(true);
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function fetchPosts() {
+            try {
+                const response = await fetch('https://cors-anywhere.herokuapp.com/https://bloggy.adaptable.app/api/v1/posts');
+                const posts = await response.json();
+
+                if (!response.ok) {
+                    throw new Error("Error fetching posts. Status: ", response.status)
+                }
+
+                setPosts(posts);
+            } catch (error) {
+                setError(error)
+                console.error(error)
+            } finally {
+                setLoadingPosts(false)
+            }
+        }
+
+        if (!ignore) {
+            fetchPosts();
+        }
+
+        return () => {
+            ignore = true;
+        }
+    }, [])
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function fetchComments() {
+            try {
+                const response = await fetch('https://cors-anywhere.herokuapp.com/https://bloggy.adaptable.app/api/v1/comments?limit=3&sort=desc');
+                const comments = await response.json();
+
+                if (!response.ok) {
+                    throw new Error("Error fetching comments. Status: ", response.status)
+                }
+
+                setComments(comments);
+            } catch (error) {
+                setError(error)
+                console.error(error)
+            } finally {
+                setLoadingComments(false);
+            }
+        }
+
+        if (!ignore) {
+            fetchComments();
+        }
+
+        return () => {
+            ignore = true;
+        }
+    }, [])
+
+    console.log(posts, comments)
+    
+    if (error) {
+        return 'A network error has occured.'
+    }
 
     return (
         <>
         <div className="posts">
-            {posts.map((post) => {
-                return <div key={post.id}>{post.title}</div> 
-                // use a HomePost component
-            })}
+            {loadingPosts ? 'Loading posts...' : (
+                posts.map((post) => {
+                    return <div key={post.id}>{post.title}</div> 
+                    // use a HomePost component
+                })
+            )}
         </div>
         <div className="recentComments">
-            {comments.map((comment) => {
-                return <div key={comment.id}>{comment.content}</div>
-                // Use a HomeComment component
-            })}
+            {loadingComments ? 'Loading recent comments...' : (
+                comments.map((comment) => {
+                    return <div key={comment.id}>{comment.content}</div>
+                    // Use a HomeComment component
+                })
+            )}
         </div>
         </>
     )
